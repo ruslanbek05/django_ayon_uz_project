fed181f6febe22532c5f00969dc93e07c0a91cd3
Ruslan <ruslanbek05@gmail.com>
2021-10-18 14:14:41 +0500

    mysql

--- django_ayon_uz_project/settings.py
+++ django_ayon_uz_project/settings.py
@@ -75,11 +75,16 @@ WSGI_APPLICATION = 'django_ayon_uz_project.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'django_ayon_uz_db',
+       'HOST': '127.0.0.1',
+       'PORT': '3306',
+       'USER': 'admin',
+       'PASSWORD': '',
+   }
 }
+
 
 
 # Password validation
