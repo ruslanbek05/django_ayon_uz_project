b8864f6c3117cb787c30c45f3d6d2909e75952e0
Ruslan <ruslanbek05@gmail.com>
2021-10-21 22:55:14 +0500

    test
    pip install coverage
    coverage run --omit='*/venv/*' manage.py test
    coverage html

--- django_ayon_uz_project/settings.py
+++ django_ayon_uz_project/settings.py
@@ -127,3 +127,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'django_static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',
+    ]
+}
