a9fcbbaac1cda1ba6d1a6a1b39e8478b8048175b
Ruslan <ruslanbek05@gmail.com>
2021-10-27 18:52:10 +0500

    ignore

--- django_ayon_uz_project/settings.py
+++ django_ayon_uz_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#!xl^ecj89e0x$epht*hs)c(x$#e=32z*cik7jq$vlo20(hzi%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["desktop-liricur", 'ayon.uz', 'www.ayon.uz', '34.135.80.3', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ayon.uz', 'www.ayon.uz', '34.135.80.3', '127.0.0.1', 'localhost', "desktop-liricur"]
 
 # Application definition
 
