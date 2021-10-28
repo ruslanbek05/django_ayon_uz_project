9c705528e1c576fc98984cfe37a220eb8296ddf3
Ruslan <ruslanbek05@gmail.com>
2021-10-28 12:01:52 +0500

    api docs

--- django_ayon_uz_project/settings.py
+++ django_ayon_uz_project/settings.py
@@ -141,7 +141,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-    )
+    ),
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema'
 }
 # Permissions:
 # AllowAny
--- django_ayon_uz_project/urls.py
+++ django_ayon_uz_project/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+from rest_framework.documentation import include_docs_urls
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -14,4 +16,10 @@ urlpatterns = [
     path('django/api/user/', include('users.urls', namespace='users')),
     path('django/api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('django/', include('blog.urls', namespace='blog')),
+    path('django/docs/', include_docs_urls(title='BlogAPI')),
+    path('django/schema', get_schema_view(
+        title="BlogAPI",
+        description="API for the BlogAPI",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
