adae39567cb3c868e7e22f74353413e902471112
Ruslan <ruslanbek05@gmail.com>
2021-10-29 22:49:04 +0500

    0

--- blog_api/views.py
+++ blog_api/views.py
@@ -47,13 +47,14 @@ class CreatePost(APIView):
 
     def post(self, request, format=None):
         print(request.data)
-        return Response(request.data, status=status.HTTP_408_REQUEST_TIMEOUT)
-        # serializer = PostSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data, status=status.HTTP_200_OK)
-        # else:
-        #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        # return Response('fff', status=status.HTTP_408_REQUEST_TIMEOUT)
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            # return Response('fff', status=status.HTTP_408_REQUEST_TIMEOUT)
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class AdminPostDetail(generics.RetrieveAPIView):
